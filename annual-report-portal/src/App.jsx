72dc0e9ef836a9150501aa2568a8a3229e32d7be
Omji-Kesharwani <omjikesharwani000@gamil.com>
2024-09-06 13:26:39 +0530

    Modified

--- annual-report-portal/src/App.jsx
+++ annual-report-portal/src/App.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
-import Header from './components/Header';
-import MainBanner from './components/MainBanner';
-import FeaturedSections from './components/FeaturedSections';
-import AdditionalCharts from './components/AdditionalCharts';
-import Footer from './components/Footer';
-import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Login-components/Navbar"
+import Home from './Login-components/Home';
+import Login from './Login-components/Login';
+import Signup from './Login-components/Signup';
+import Dashboard from './Login-components/Dashboard';
+import { useState } from 'react';
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <MainBanner />
-            <FeaturedSections />
-            {/* <AdditionalCharts /> */}
-            <Footer />
-        </div>
-    );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  return (
+    <div className="w-screen h-[100vh] bg-[#F5F5F5] flex flex-col ">
+      <Router>
+        <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- annual-report-portal/src/Login-components/Signup.jsx
+++ annual-report-portal/src/Login-components/Signup.jsx
@@ -6,9 +6,9 @@ import Template from './Template';
 function Signup({ setIsLoggedIn }) {
   return (
     <Template
-      title="Join the millions learning to code with StudyNotion for free"
-      desc1="Build skills for today, tomorrow, and beyond."
-      desc2="Education to future-proof your career."
+      title=""
+      desc1=""
+      desc2=""
       image={signupImg}
       formType="signup"
       setIsLoggedIn={setIsLoggedIn}
